25e79e238e97d236db1e3d94c95af4e786c60cb0
Beletskiy <Alexey1Beletskiy@gmail.com>
2015-05-22 16:23:24 +0300

    second commit

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -7,8 +7,8 @@ $this->title = 'Bones Yii';
     <div class="jumbotron">
         <h1>Игра Кости</h1><br>
         
-        <p><a class="btn btn-lg btn-success" href="http://localhost/basic/web/index.php?r=results/index">Добавить новый эксперимент</a></p>
-        <p><a class="btn btn-lg btn-success" href="http://localhost/basic/web/index.php?r=experiment/index">Просмотр статистики экспериментов</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://localhost/beletskiy/Yii_Task1_Practice1/basic/web/index.php?r=results/index">Добавить новый эксперимент</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://localhost/beletskiy/Yii_Task1_Practice1/basic/web/index.php?r=experiment/index">Просмотр статистики экспериментов</a></p>
     </div>
 
 </div>
